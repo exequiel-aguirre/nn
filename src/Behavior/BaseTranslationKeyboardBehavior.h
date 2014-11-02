0e7ac889e8e8f9e146cd9fb4be45f32feb613f36
exe <exe@zotac>
2014-11-02 04:46:31 -0300

    now the wasd keys take in account the thetha and phi.Still capturing mouse motion on every frame(bug)

--- src/Behavior/BaseTranslationKeyboardBehavior.h
+++ src/Behavior/BaseTranslationKeyboardBehavior.h
@@ -2,6 +2,7 @@
 #define BaseTranslationKeyboardBehaviorH
 #include "../Component/Component.h"
 #include "../Behavior/AbstractBehavior.h"
+#include <math.h>
 
 /**
  * This behavior should be added by default to each "decorative" 3d object, in order to create
@@ -18,39 +19,52 @@ class BaseTranslationKeyboardBehavior: public AbstractBehavior{
     
     //todo:make an "function" object with the responsability of generating the new position
     void  onKeyDown(SDL_Keycode key){
-    	float deltaX=0.0f;    	
+    	float deltaX=0.0f;
+        float deltaY=0.0f;    	
     	float deltaZ=0.0f;
-    	//TODO:do not commit this awful flag!
+        float delta=0.0f;
+        Position* currentPosition=getComponent()->getPosition();
+    	//TODO:do not commit this awful flag!define the flag on the default case
+        //REFACTOR!!!(asociate w with 0*PI,d->PI/2,s->PI,a->-PI/2)
     	bool shouldUpdatePosition=false;
 
     	switch(key){ 
-    	case SDLK_w:{
-    		deltaZ+=DEFAULT_SPEED;
-    		shouldUpdatePosition=true;
-    		break;
-    	}
-    	case SDLK_s:{
-    		deltaZ+=-DEFAULT_SPEED;
-    		shouldUpdatePosition=true;
-    		break;
-    	}
-    	case SDLK_d:{
-    		deltaX+=-DEFAULT_SPEED;
-    		shouldUpdatePosition=true;
-    		break;
-    	}
-    	case SDLK_a:{
-    		deltaX+=DEFAULT_SPEED;
-    		shouldUpdatePosition=true;
-    		break;
-    	}
-    	default:{
-    		break;
-    	}
+        	case SDLK_w:{        		
+                deltaX=DEFAULT_SPEED * -sinf(currentPosition->getAbsoluteTheta() * M_PI/180);//change from grads to rads and calculate the x component
+                deltaZ=DEFAULT_SPEED * cosf(currentPosition->getAbsoluteTheta() * M_PI/180);//change from grads to rads and calculate the x component
+                deltaY=sinf(currentPosition->getAbsolutePhi() * M_PI/180) * DEFAULT_SPEED;//change from grads to rads and calculate the x component           
+        		shouldUpdatePosition=true;
+        		break;
+        	}
+        	case SDLK_s:{
+        		deltaX=-DEFAULT_SPEED * -sinf(currentPosition->getAbsoluteTheta() * M_PI/180);//change from grads to rads and calculate the x component
+                deltaZ=-DEFAULT_SPEED * cosf(currentPosition->getAbsoluteTheta() * M_PI/180);//change from grads to rads and calculate the x component
+                deltaY=sinf(currentPosition->getAbsolutePhi() * M_PI/180) * -DEFAULT_SPEED;//change from grads to rads and calculate the x component           
+        		shouldUpdatePosition=true;
+        		break;
+        	}
+        	case SDLK_d:{
+        		deltaX=-DEFAULT_SPEED * cosf(currentPosition->getAbsoluteTheta() * M_PI/180);//change from grads to rads and calculate the x component
+                deltaZ=-DEFAULT_SPEED * sinf(currentPosition->getAbsoluteTheta() * M_PI/180);//change from grads to rads and calculate the x component
+        		shouldUpdatePosition=true;
+        		break;
+        	}
+        	case SDLK_a:{
+        		deltaX=DEFAULT_SPEED * cosf(currentPosition->getAbsoluteTheta() * M_PI/180);//change from grads to rads and calculate the x component
+                deltaZ=DEFAULT_SPEED * sinf(currentPosition->getAbsoluteTheta() * M_PI/180);//change from grads to rads and calculate the x component
+        		shouldUpdatePosition=true;
+        		break;
+        	}
+        	default:{
+        		break;
+        	}
     	}
     	if(shouldUpdatePosition){
-    		Position* currentPosition=getComponent()->getPosition();
-    		Position* newPosition=new Position(currentPosition->getAbsoluteX()+deltaX,currentPosition->getAbsoluteY(),currentPosition->getAbsoluteZ()+deltaZ,
+
+            Position* newPosition=new Position(
+                currentPosition->getAbsoluteX()+deltaX,
+                currentPosition->getAbsoluteY()+deltaY,
+                currentPosition->getAbsoluteZ()+deltaZ,
                 currentPosition->getAbsoluteTheta(),currentPosition->getAbsolutePhi());
     		getComponent()->setPosition(newPosition);
     	}
