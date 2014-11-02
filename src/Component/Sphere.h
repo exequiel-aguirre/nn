977d507b4e4bf8862891516b1d153751f716e09c
exe <exe@zotac>
2014-11-02 17:59:41 -0300

    adding the sphere. no glu.Check where I really use glu

---
+++ src/Component/Sphere.h
@@ -0,0 +1,71 @@
+#ifndef SphereH
+#define SphereH
+
+#include "Component.h"
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+
+class Sphere: public Component {  
+  private:
+	GLuint	texture;
+	char* defaultTextureFilename="img/box.bmp";
+	
+	void loadTexture(char* fileName){
+		SDL_Surface* bmpFile=SDL_LoadBMP(fileName);		
+		if(bmpFile == NULL ) std::cout <<  SDL_GetError();
+		glGenTextures(1,&texture);
+		glBindTexture(GL_TEXTURE_2D,texture);
+		glTexImage2D(GL_TEXTURE_2D,0,1,bmpFile->w,bmpFile->h,0,GL_RGB,GL_UNSIGNED_BYTE,bmpFile->pixels);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+		
+		SDL_FreeSurface(bmpFile);
+	}
+	
+  public:
+	  Sphere(Position* position,char* textureFilename):Component(position){
+		  if(textureFilename==NULL) textureFilename=defaultTextureFilename;
+		  loadTexture(textureFilename);		  
+	  }		
+
+    virtual ~Sphere(){}
+    
+    void render(){
+			
+			//pull this up			
+			glTranslatef(this->position->getAbsoluteX(),this->position->getAbsoluteY(),this->position->getAbsoluteZ());	
+
+			float r=1.0;
+			float theta;
+			float phi0;
+			float phi1;
+			int lats=24;
+			int longs=24;
+			int i, j;
+        	for(i = 0; i <= lats; i++) 
+        	{
+	           phi0= -(M_PI/2) + ((M_PI/lats)* (i-1));
+	           phi1= -(M_PI/2) + ((M_PI/lats)* i);
+
+	           glBegin(GL_QUAD_STRIP);
+		           for(j = 0; j <= longs; j++) 
+		           {   
+					   theta=(2*M_PI/longs) * (j-1);
+					   glNormal3f(r*cos(theta)*sin(phi0), r*sin(theta)*sin(phi0),r*cos(phi0));
+		               glVertex3f(r*cos(theta)*sin(phi0), r*sin(theta)*sin(phi0),r*cos(phi0));
+		               
+		               glNormal3f(r*cos(theta)*sin(phi1), r*sin(theta)*sin(phi1),r*cos(phi1));		            
+		               glVertex3f(r*cos(theta)*sin(phi1), r*sin(theta)*sin(phi1),r*cos(phi1));
+		           }
+	           glEnd();
+			}
+			//we restore the position to avoid messing with the other's component's location
+			//we can always draw them directly using the position on the glvertexinstruction above...
+		   glTranslatef(-this->position->getAbsoluteX(),-this->position->getAbsoluteY(),-this->position->getAbsoluteZ());	
+    }
+
+};
+
+
+#endif
--- src/Test/MyContainer.h
+++ src/Test/MyContainer.h
@@ -2,6 +2,7 @@
 #define MyContainerH
 
 #include "../Component/Container.h"
+#include "../Component/Sphere.h"
 #include "../Component/Box.h"
 #include "../Component/Ground.h"
 #include "../Component/Camera.h"
@@ -13,11 +14,13 @@ class MyContainer:public Container{
     MyContainer(Position* position):Container(position){
 			Box* my3DBox=new Box(new Position(0.0f,0.0f,-19.0f),NULL);
 			Box* my3DBox2=new Box(new Position(-3.0f,0.0f,-19.0f),NULL);
+			Sphere* mySphere=new Sphere(new Position(0.0f,0.0f,-7.0f),NULL);
 			Ground* myGround=new Ground(new Position(0.0f,-1.0f,0.0f));
 			Camera* myCamera=new Camera(new Position(0.0f,-1.0f,0.0f));			
 			
 			add(my3DBox);
 			add(my3DBox2->add(new TimeBehavior()));
+			add(mySphere);
 			add(myGround);
 			add(myCamera);
 			//add(my3DBox2);
