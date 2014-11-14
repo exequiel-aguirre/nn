cff8f29d5af8b0f2d1e730e6f3e87f7b170bda83
exe <exe@zotac>
2014-11-13 22:29:29 -0300

    adding Point. This one worked with glbufferdata

---
+++ src/Utils/Point.h
@@ -0,0 +1,26 @@
+#ifndef PointH
+#define PointH
+
+class Point{
+  
+  private:
+   float x;//put it in one line
+   float y;
+   float z;
+  public:
+	
+	Point(float x,float y,float z){
+		this->x=x;
+		this->y=y;
+		this->z=z;
+	}
+	//virtual ~Point(){} do not use virtual because it messes up the glvertexbuff
+
+	float &operator[](int i){
+		if(i==0) return x;
+		if(i==1) return y;
+		if(i==2) return z;
+	}
+};
+
+#endif
