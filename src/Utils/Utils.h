3b95d88822b2776556351f57d280b044b6cb93ce
exe <exe@zotac>
2014-11-20 20:20:42 -0300

    bugfix:the textures loading is working

--- src/Utils/Utils.h
+++ src/Utils/Utils.h
@@ -17,7 +17,7 @@ class Utils{
 		if(bmpFile == NULL ) std::cout <<  SDL_GetError();
 		glGenTextures(1,&texture);
 		glBindTexture(GL_TEXTURE_2D,texture);
-		glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,bmpFile->w,bmpFile->h,0,GL_RGB,GL_UNSIGNED_BYTE,bmpFile->pixels);
+		glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,bmpFile->w,bmpFile->h,0,GL_BGR,GL_UNSIGNED_BYTE,bmpFile->pixels);
 		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);		
 		SDL_FreeSurface(bmpFile);
