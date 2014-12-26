421411cb2927a88e54912e4808f6911600a38e92
exe <exe@zotac>
2014-12-26 08:00:55 -0300

    stitching the sphere seems to fix the problem of the decimation making holes. With the bunny(without stitching it also seems to work fine=>problem with my surfaces)

--- src/DataStructure/DecimatedPolygon.h
+++ src/DataStructure/DecimatedPolygon.h
@@ -4,7 +4,6 @@
 #include <vector>
 #include "DecimatedDataStructure.h"
 
-
 class DecimatedPolygon{
   
   private:
@@ -12,14 +11,18 @@ class DecimatedPolygon{
     vector<vector<int>> indexedTriangles;    
     //just for debugging
     vector<Point> vertices;
+    const float EPSILON=0.0000001;
   public:	   
 	 
     DecimatedPolygon(vector<Point> vertices){
       build(vertices);
+      stitch();
       this->vertices=nnd::decimate(indexedVertices,indexedTriangles);
       //buildVertices();
   	}
+
     void build(vector<Point> vertices){
+
       indexedVertices=vertices;
       //remove duplicated points
       std::sort( indexedVertices.begin(), indexedVertices.end() );
@@ -29,6 +32,7 @@ class DecimatedPolygon{
       vector<Point>::iterator it;
       for(it=vertices.begin();it!=vertices.end();it+=3){
         if((*it)==(*(it+1)) || (*it)==(*(it+2)) || (*(it+1))==(*(it+2))) continue;//malformed triangle
+        if(distance((*it),*(it+1))<EPSILON || distance((*it),*(it+2))<EPSILON || distance(*(it+1),*(it+2))<EPSILON) continue;
         vector<int> indexedTriangle;
         indexedTriangle.push_back(getIndex(*it));
         indexedTriangle.push_back(getIndex(*(it+1)));
@@ -45,6 +49,33 @@ class DecimatedPolygon{
     }
 
 
+  void stitch(){
+      //first we build a vector with which indexes are going to be removed(and replaced by another close by)
+      vector<vector<int>> removedIndexes;
+      for (int i=0;i<indexedVertices.size();i++){
+        for(int j=i+1;j<indexedVertices.size();j++){
+          if(distance(indexedVertices[i],indexedVertices[j])<EPSILON){
+            vector<int> removed;
+            removed.push_back(j);//item j is
+            removed.push_back(i);//replaced by item i
+            removedIndexes.push_back(removed);
+          }//Since this is ordered here we can put an else{break;}
+        }
+      }
+      //we do the actual replace
+      for(int i=0;i<indexedTriangles.size();i++){
+        for(int j=0;j<removedIndexes.size();j++){
+            if(indexedTriangles[i][0]==removedIndexes[j][0]) indexedTriangles[i][0]=removedIndexes[j][1];
+            if(indexedTriangles[i][1]==removedIndexes[j][0]) indexedTriangles[i][1]=removedIndexes[j][1];
+            if(indexedTriangles[i][2]==removedIndexes[j][0]) indexedTriangles[i][2]=removedIndexes[j][1];
+          }
+        }
+    }
+
+    float distance(Point& p1,Point& p2){
+      return (p2-p1).norm();
+    }
+
     void buildVertices(){
       for(int i=0;i<indexedTriangles.size();i++){
         vertices.push_back(indexedVertices[indexedTriangles[i][0]]);
@@ -52,6 +83,7 @@ class DecimatedPolygon{
         vertices.push_back(indexedVertices[indexedTriangles[i][2]]);
       }
     }
+
     vector<Point>& getVertices(){
       return vertices;
     }
