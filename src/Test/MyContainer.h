203afb15522aecb7bbcee6014b496001d6477f7b
exe <exe@zotac>
2015-01-03 19:44:04 -0300

    a hack to fix the model's physics and some comments

--- src/Test/MyContainer.h
+++ src/Test/MyContainer.h
@@ -25,7 +25,7 @@
 class MyContainer:public Container{
   public:
     MyContainer(Position&& position):Container(position){
-			/*Box* my3DBox=new Box(Position(3.0f,0.0f,-25.0f));
+			Box* my3DBox=new Box(Position(3.0f,0.0f,-25.0f));
 			Box* my3DBox2=new Box(Position(-3.0f,0.0f,-25.0f));
 			Hut* myHut=new Hut(Position(25.0f,0.0f,25.0f));
 			Mill* myMill=new Mill(Position(35.0f,0.0f,-35.0f));
@@ -43,18 +43,18 @@ class MyContainer:public Container{
 			Water* myWaterB=new Water(Position(0.0f,0.0f,100.0f),100,100);
 			Water* myWaterL=new Water(Position(-100.0f,0.0f,0.0f),100,300);
 			Model* myModel=new Model(Position(-15.0f,0.0f,-19.0f),"3DModel/monkey.obj");
-			Animation* myAnimation=new Animation(Position(0.0f,0.4f,-0.0f),"3DModel/human.obj");*/
+			Animation* myAnimation=new Animation(Position(0.0f,0.4f,-0.0f),"3DModel/human.obj");
 			Camera* myCamera=new Camera(Position(0.0f,-2.0f,0.0f));
 			Light* myLight=new Light(Position(40.0f,40.0f,40.0f));
-			/*Sphere* mySphere=new Sphere(Position(0.0f,4.0f,-10.0f),0.39f);
+			Sphere* mySphere=new Sphere(Position(0.0f,4.0f,-10.0f),0.39f);
 			Sphere* mySphere2=new Sphere(Position(-3.0f,8.0f,-20.0f),0.5f);
-			Sphere* mySphere3=new Sphere(Position(3.0f,7.0f,-20.0f),2.0f);*/
+			Sphere* mySphere3=new Sphere(Position(3.0f,6.0f,-20.0f),2.0f);
 			Sphere* mySphere4=new Sphere(Position(-18.0f,20.0f,-17.0f),0.5f);
 			
 			Physics* physics=new Physics();
 			
 			
-			/*add(my3DBox);
+			add(my3DBox);
 			add(my3DBox2->add(new MotionBehavior()));			
 			add(myHut);
 			add(myMill);
@@ -64,12 +64,12 @@ class MyContainer:public Container{
 			add(myTree1);add(myTree2);add(myTree3);add(myTree4);add(myTree5);add(myTree6);
 			add(myWaterF);add(myWaterR);add(myWaterB);add(myWaterL);
 			add(myModel->add(new MotionBehavior()));
-			myAnimation->getVelocity().setZ(8.0f);add(myAnimation->add(new MotionBehavior()));*/
+			myAnimation->getVelocity().setZ(8.0f);add(myAnimation->add(new MotionBehavior()));
 			add(myCamera);
 			add(myLight);
-			/*add(mySphere->add(new MotionBehavior()));
+			add(mySphere->add(new MotionBehavior()));
 			mySphere2->getVelocity().setX(40.0f);add(mySphere2->add(new MotionBehavior()));
-			add(mySphere3->add(new MotionBehavior()));*/
+			add(mySphere3->add(new MotionBehavior()));
 			add(mySphere4->add(new MotionBehavior()));
 
 
