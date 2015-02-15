0564f675538668acb618bf0973b853d3cc660f9e
exe <exe@zotac>
2015-02-15 16:18:56 -0300

    light is back

--- src/RenderStrategy/Shader/Basic.fs
+++ src/RenderStrategy/Shader/Basic.fs
@@ -1,5 +1,6 @@
 #version 120
 
+varying vec3 vertex0;
 varying vec2 uv0;
 varying vec3 normal0;
 
@@ -7,7 +8,24 @@ uniform sampler2D sampler;
 
 void main()
 {
-	//gl_FragColor=vec4(1.0,0.0,0.0,1.0);
-	gl_FragColor = texture2D(sampler, uv0)*
-		clamp(dot(vec3(-0.5,0.5,0.5), normal0), 0.0, 1.0);
+	//Phong shading model
+	vec3 L = normalize(gl_LightSource[0].position.xyz - vertex0);   
+	vec3 E = normalize(-vertex0);
+	vec3 R = normalize(-reflect(L,normal0));  
+
+	//Ambient term
+	vec4 Iamb = gl_FrontLightProduct[0].ambient;    
+
+	//diffuse term
+	vec4 Idiff = gl_FrontLightProduct[0].diffuse * max(dot(normal0,L), 0.0);
+	Idiff = clamp(Idiff, 0.0, 1.0);     
+
+	// specular term
+	vec4 Ispec = gl_FrontLightProduct[0].specular 
+		* pow(max(dot(R,E),0.0),0.3*gl_FrontMaterial.shininess);
+	Ispec = clamp(Ispec, 0.0, 1.0); 
+
+	// the texture affected by all the previous terms
+	gl_FragColor = texture2D(sampler, uv0) *
+		(gl_FrontLightModelProduct.sceneColor + Iamb + Idiff + Ispec); 
 }
--- src/RenderStrategy/Shader/Basic.vs
+++ src/RenderStrategy/Shader/Basic.vs
@@ -4,12 +4,15 @@ attribute vec3 vertex;
 attribute vec2 uv;
 attribute vec3 normal;
 
+varying vec3 vertex0;
 varying vec2 uv0;
 varying vec3 normal0;
 
 void main()
 {
 	gl_Position=gl_ModelViewProjectionMatrix * vec4(vertex, 1.0);
+
+	vertex0 = vec3(gl_ModelViewMatrix * vec4(vertex,1.0));
 	uv0 = uv;	
-	normal0 = normal;
+   	normal0 = normalize(gl_NormalMatrix * normal);
 }
