dfecab86d9cf1c5c0ff068c8d55b07e3342eae05
exe <exe@zotac>
2015-02-20 11:39:03 -0300

    minor change.Making solid the default for a couple surfaces

--- src/Component/Sphere.h
+++ src/Component/Sphere.h
@@ -10,10 +10,10 @@ class Sphere: public Surface {
   private:
 	const float DEFAULT_R=1.0f;
   public:
-	Sphere(Position&& position):Surface(position,EllipsoidMap(DEFAULT_R,DEFAULT_R,DEFAULT_R),GL_LINES){
+	Sphere(Position&& position):Surface(position,EllipsoidMap(DEFAULT_R,DEFAULT_R,DEFAULT_R),GL_TRIANGLES){
     this->rotates=true;
   }
-	Sphere(Position&& position,float r):Surface(position,EllipsoidMap(r,r,r),GL_LINES){
+	Sphere(Position&& position,float r):Surface(position,EllipsoidMap(r,r,r),GL_TRIANGLES){
     this->rotates=true;
   }
 	Sphere(Position&& position,float r,char* textureFilename):Surface(position,EllipsoidMap(r,r,r),textureFilename){
--- src/Component/Torus.h
+++ src/Component/Torus.h
@@ -12,8 +12,8 @@ class Torus: public Surface {
 	const float DEFAULT_R2=1.0f;
   public:
 	  
-	Torus(Position&& position):Surface(position,TorusMap(DEFAULT_R1,DEFAULT_R2),GL_LINES){}
-	Torus(Position&& position,float r1,float r2):Surface(position,TorusMap(r1,r2),GL_LINES){}
+	Torus(Position&& position):Surface(position,TorusMap(DEFAULT_R1,DEFAULT_R2),GL_TRIANGLES){}
+	Torus(Position&& position,float r1,float r2):Surface(position,TorusMap(r1,r2),GL_TRIANGLES){}
 	Torus(Position&& position,float r1,float r2,char* textureFilename):Surface(position,TorusMap(r1,r2),textureFilename){}
 
     virtual ~Torus(){}    
