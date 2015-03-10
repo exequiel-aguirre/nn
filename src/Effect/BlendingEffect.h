05da77042f44b163f6e7545208a1a6ad9df11230
exe <exe@zotac>
2015-03-10 10:21:15 -0300

    removing a deprecated call that seemed to have no effect whatsoever

--- src/Effect/BlendingEffect.h
+++ src/Effect/BlendingEffect.h
@@ -13,7 +13,6 @@ class BlendingEffect:public IEffect{
 	void doEffect(){
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA,GL_ONE);
-		glColor4f(0.5f, 0.5f, 0.9f, 0.5f);
 	}
 	void undoEffect(){
 		glDisable(GL_BLEND);
