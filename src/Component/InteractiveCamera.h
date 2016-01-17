14e5d2a9e0aa5f357599db2b6202a71042fbdc25
exe <exe@zotac>
2016-01-17 19:58:22 -0300

    removing climbing and friction logic

--- src/Component/InteractiveCamera.h
+++ src/Component/InteractiveCamera.h
@@ -15,12 +15,8 @@ class InteractiveCamera: public Camera {
     vector<Weapon*> weapons;
     vector<Weapon*>::iterator currentWeapon;
   protected:
-    float elasticity=0.0f;
-    const float U_D=0.05;
-    float u_d=0.0;
     float headOffset=1.0;//offset on the y axis of the camera view.(should not affect physics)
     float gamma=0.0;
-    bool climbing=false;
   public:
 	  InteractiveCamera(Position position):Camera(position,false,[=](float deltaX,float deltaY,float deltaZ){ this->onTranslation(deltaX,deltaY,deltaZ);},ModelObject(EllipsoidMap(1.0,2.0,1.0)),GL_POINTS){
           //enable physics(both, the this line and a modelObject with a geometry are needed)
@@ -62,27 +58,15 @@ class InteractiveCamera: public Camera {
 
     void setPosition(float x,float y,float z,float phi,float theta,float psi){
       Camera::setPosition(x,y,z,phi,theta,psi);
-      //Climbing logic(part 2)
-      if(climbing && !getCollisionStatus().hasCollided()) {//finalize the climbing by moving towards
-        float deltaX = 2.5 *-sinf(theta * M_PI/180);
-        float deltaZ = 2.5 * cosf(theta * M_PI/180);
-        this->setVelocity(velocity.getX()-deltaX,velocity.getY(),velocity.getZ()-deltaZ);
-        climbing=false;
-      }
-
       for(unsigned int i=0;i<weapons.size();i++) (weapons[i])->setPosition(x,y,z,phi,theta,psi);
     }
     void onTranslation(float deltaX,float deltaY,float deltaZ){
       if(getCollisionStatus().hasCollided()){
-        //We take in account the normal, to avoid penetration.
         Point v=Point(-deltaX*20,-deltaY*20,-deltaZ*20);
-        //Point n=getCollisionStatus().getImpactNormal();
-        //v=v- ((v*n)*n);
         this->setVelocity(v.x,v.y,v.z);
       }
     }
     void onKeyDown(SDL_Keycode key){
-        this->u_d=0.0;//disable friction
         switch(key){
           case SDLK_SPACE:
           {
@@ -107,7 +91,6 @@ class InteractiveCamera: public Camera {
         }
     }
      void onKeyUp(SDL_Keycode key){
-        this->u_d=U_D;//enable friction
         if(getCollisionStatus().hasCollided()) this->setVelocity(0,0,0);
      }
 
@@ -120,22 +103,6 @@ class InteractiveCamera: public Camera {
       }
     }
 
-    void onAfterCollision(){
-      Velocity v=this->getVelocity();// * (1.0-this->u_d);
-      this->setVelocity(v.getX(),v.getY(),v.getZ());
-      //Climbing logic(part 1)
-      //TODO:put this setVelocity just once
-      Point normal=getCollisionStatus().getImpactNormal();
-      if( fabs(normal*Point(0,1,0)) <0.0001){//we start climbing
-        this->setVelocity(0,v.getY()+0.7,0);
-        climbing=true;
-      }else if(climbing){//we reached the edge, so some final jump.(not always detected)
-        this->setVelocity(0,v.getY()+10.0,0);
-      }
-
-
-    }
-
     //Move the head up and down
     void onWalk(){
       gamma+=0.5;
@@ -145,9 +112,6 @@ class InteractiveCamera: public Camera {
     void calculateBoundary(){
       //update the boundary
       getBoundary().update(this->modelMatrix.getTranslationMatrix(),velocity);
-    }
-    float getElasticity(){
-      return elasticity;
     }
 };
 
