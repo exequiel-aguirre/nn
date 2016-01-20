e1b2d21888b6f27304dc0805404d528ba617f5aa
exe <exe@zotac>
2016-01-20 19:20:54 -0300

    Adding mipmaps. This seems to fix the drop on framerate near the building

--- src/Utils/Utils.h
+++ src/Utils/Utils.h
@@ -48,8 +48,9 @@ class Utils{
 		glGenTextures(1,&texture);
 		glBindTexture(GL_TEXTURE_2D,texture);
 		glTexImage2D(GL_TEXTURE_2D,0,internalFormat,w,h,0,format,GL_UNSIGNED_BYTE,pixels);
-		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);		
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR_MIPMAP_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 		glBindTexture(GL_TEXTURE_2D,0);//unbind
