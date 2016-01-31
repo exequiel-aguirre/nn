89fd6829d8ea80f54c4421abd763a480777e6e0e
exe <exe@zotac>
2016-01-31 17:35:56 -0300

    making blades thin boxes instead of planes

--- src/Component/Mill.h
+++ src/Component/Mill.h
@@ -4,7 +4,7 @@
 #include "Container.h"
 #include "../Map/ConeMap.h"
 #include "../Map/CylinderMap.h"
-#include "Plane.h"
+#include "Box.h"
 #include "../Behavior/MotionBehavior.h"
 
 
@@ -15,11 +15,11 @@ class Mill: public Container {
 	  	  
 		add(new Surface(Position(0.0f,10.0f,0.0f),ConeMap(3.0f,15.0f),"img/hut_roof.bmp"));
 		 
-		Plane* blade1=new Plane(Position(0.0f,9.0f,1.5f,90.0f,0.0f,45.0f),10.0f,2.0f,"img/hut_wall.bmp");
+		Box* blade1=new Box(Position(0.0f,9.0f,1.5f,90.0f,0.0f,45.0f),10.0f,2.0f,0.2f,"img/hut_wall.bmp");
 		blade1->setVelocity(0,0,0,0,0.0f,90.0);
 		add(blade1->add(new MotionBehavior()));
 		
-		Plane* blade2=new Plane(Position(0.0f,9.0f,2.0f,90.0f,00.0f,-45.0f),10.0f,2.0f,"img/hut_wall.bmp");
+		Box* blade2=new Box(Position(0.0f,9.0f,2.0f,90.0f,00.0f,-45.0f),10.0f,2.0f,0.2f,"img/hut_wall.bmp");
 		blade2->setVelocity(0,0,0,0,00.0f,90.0);
 		add(blade2->add(new MotionBehavior()));
 
